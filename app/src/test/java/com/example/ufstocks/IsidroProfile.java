a0a41c99b068fc60d71774b009958d2bf2ba63da
Serrano <em.mlwn06@gmail.com>
2026-09-16 14:22:49 +0800

    asadw

---
+++ app/src/test/java/com/example/ufstocks/IsidroProfile.java
@@ -0,0 +1,21 @@
+package com.example.ufstocks;
+
+import org.junit.Test;
+
+public class IsidroProfile {
+
+    @Test
+    public void IsidroFunction() {
+        // --- 1. THE INPUT (Storing your personal details in variables) ---
+        String myName = "Keziah";
+        String nickName = "Keisha";
+        String favGame = "MLBB";
+        int myAge = 16;
+
+        // --- 2. THE OUTPUT (Printing to the console) ---
+        System.out.println("--- My Profile ---");
+        System.out.println("Hello my name is " + myName + " and my age is " + myAge + " years old.");
+        System.out.println("my Nickname are " + nickName + "and please call my that name.");
+        System.out.println("My favorite game are always " + favGame + " and i will play it everyday if i could. ");
+    }
+}
