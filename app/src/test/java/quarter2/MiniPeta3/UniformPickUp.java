98830e749d2c5f1681dac9d6750610afedc3fb7f
Serrano <em.mlwn06@gmail.com>
2026-09-18 15:08:24 +0800

    asadwASAS

---
+++ app/src/test/java/quarter2/MiniPeta3/UniformPickUp.java
@@ -0,0 +1,12 @@
+package quarter2.MiniPeta3;
+
+import java.util.Scanner;
+
+public class UniformPickUp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+
+    }
+}
