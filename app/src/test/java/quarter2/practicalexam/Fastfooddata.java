ddcd235b2d7078cf653225f25e4c9347873bc464
Alrasid <darkvoid3399@gmail.com>
2026-09-23 15:08:17 +0800

    New Fast food and fast food data, With completed codes

---
+++ app/src/test/java/quarter2/practicalexam/Fastfooddata.java
@@ -0,0 +1,32 @@
+package quarter2.practicalexam;
+
+import java.util.Scanner;
+
+public class Fastfooddata {
+
+    public void start(Scanner scanner) {
+        double price = 0;
+
+        while (true) {
+            int choice = scanner.nextInt();
+
+            if (choice == 1) {
+                price = 50;
+                System.out.println("Order selected: $75");
+            }
+            else if (choice == 2) {
+                double payment = scanner.nextDouble();
+                if (payment < price) {
+                    System.out.println("The payment is insufficient.");
+                } else {
+                    System.out.println("Payment success! Change: $" + (payment - price));
+                    price = 0;
+                }
+            }
+            else if (choice == 3) {
+                System.out.println("Exiting the Site.");
+                break;
+            }
+        }
+    }
+}
